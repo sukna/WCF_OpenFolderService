19a721cf6c9b1a609e08b3002a7f41cec399d75c
Bojan Suknović <bojan.suknovic@prointer.ba>
2021-12-10 23:01:28 +0100

    Initial

---
+++ WCF_OpenFolder/IService.cs
@@ -0,0 +1,17 @@
+﻿using System.ServiceModel;
+using System.ServiceModel.Web;
+
+namespace WCF_OpenFolder
+{
+    [ServiceContract]
+    public interface IService
+    {
+        [OperationContract]
+        [WebInvoke(Method ="GET",
+            UriTemplate = "/openFolder?path={path}",
+            RequestFormat = WebMessageFormat.Json,
+            ResponseFormat = WebMessageFormat.Json,
+            BodyStyle = WebMessageBodyStyle.Wrapped)]
+        string OpenFolder(string path);
+    }
+}
---
+++ WCF_OpenFolder/Service.svc.cs
@@ -0,0 +1,13 @@
+﻿using System.Diagnostics;
+
+namespace WCF_OpenFolder
+{
+    public class Service : IService
+    {
+        public string OpenFolder(string path)
+        {
+            Process.Start("explorer.exe", path);
+            return string.Format("You entered: {0}", path);
+        }
+    }
+}
